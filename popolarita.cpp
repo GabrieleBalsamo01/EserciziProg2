bcea109e146ffeaf4054232533145b2b53ac7219
unknown <gabrielebalsamoct@gmail.com>
2022-07-19 12:53:50 +0200

    daff

---
+++ popolarita.cpp
@@ -0,0 +1,37 @@
+/*
+Dato un file contenente un elenco di utenti nel seguente formato
+ID;COGNOME;NOME;POPOLARITA
+ID;COGNOME;NOME;POPOLARITA
+…
+ID;COGNOME;NOME;POPOLARITA
+Dove COGNOME e NOME sono stringhe e POPOLARITA è un double, implementare almeno due algoritmi di ordinamento tra quelli visti a lezione. Inoltre, sfruttando le 
+librerie standard, valutarne la complessità temporale stimando il tempo di esecuzione.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class Lista 
+{
+public:
+	int id;
+	double popolarita;
+	string nome;
+	string cognome;	
+		
+	Lista() {}
+	
+};
+
+lettura()
+{
+	
+}
+
+
+
+int main()
+{
+	
+}
